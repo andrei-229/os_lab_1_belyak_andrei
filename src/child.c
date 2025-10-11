aee4587b2ee5ebc9585ffe567c48de0630c2c54e
andrei-229 <andrejbelak708@gmail.com>
2025-10-11 23:46:53 +0300

    use only read and write

--- src/child.c
+++ src/child.c
@@ -1,59 +1,147 @@
 #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <unistd.h>
+
+// int main(int argc, char *argv[])
+// {
+//     if (argc != 2)
+//     {
+//         fprintf(stderr, "Usage: child <filename>\n");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     FILE *file = fopen(argv[1], "w");
+//     if (file == NULL)
+//     {
+//         perror("fopen");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     char line[1024];
+//     while (fgets(line, sizeof(line), stdin) != NULL)
+//     {
+//         float numbers[100];
+//         int count = 0;
+
+//         char *token = strtok(line, " \n");
+//         while (token != NULL && count < 100)
+//         {
+//             numbers[count++] = atof(token);
+//             token = strtok(NULL, " \n");
+//         }
+
+//         if (count < 2)
+//         {
+//             fprintf(stderr, "Error: Need at least 2 numbers\n");
+//             continue;
+//         }
+
+//         float dividend = numbers[0];
+//         for (int i = 1; i < count; i++)
+//         {
+//             if (numbers[i] == 0.0f)
+//             {
+//                 fprintf(file, "Division by zero detected. Exiting.\n");
+//                 fflush(file);
+//                 int status_err = -1;
+//                 fclose(file);
+//                 exit(EXIT_FAILURE);
+//             }
+//             float result = dividend / numbers[i];
+//             fprintf(file, "%.2f / %.2f = %.2f\n", dividend, numbers[i], result);
+//         }
+//         fflush(file);
+//     }
+
+//     fclose(file);
+//     return 0;
+// }
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+void write_string(int fd, const char *str)
+{
+    write(fd, str, strlen(str));
+}
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        fprintf(stderr, "Usage: child <filename>\n");
+        write_string(STDERR_FILENO, "Usage: child <filename>\n");
         exit(EXIT_FAILURE);
     }
 
-    FILE *file = fopen(argv[1], "w");
-    if (file == NULL)
+    int file = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (file == -1)
     {
-        perror("fopen");
+        write_string(STDERR_FILENO, "open error\n");
         exit(EXIT_FAILURE);
     }
 
+    char buffer[1024];
+    ssize_t bytes_read;
     char line[1024];
-    while (fgets(line, sizeof(line), stdin) != NULL)
+    int line_pos = 0;
+
+    while ((bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0)
     {
-        float numbers[100];
-        int count = 0;
-
-        char *token = strtok(line, " \n");
-        while (token != NULL && count < 100)
+        for (int i = 0; i < bytes_read; i++)
         {
-            numbers[count++] = atof(token);
-            token = strtok(NULL, " \n");
-        }
-
-        if (count < 2)
-        {
-            fprintf(stderr, "Error: Need at least 2 numbers\n");
-            continue;
-        }
-
-        float dividend = numbers[0];
-        for (int i = 1; i < count; i++)
-        {
-            if (numbers[i] == 0.0f)
+            if (buffer[i] == '\n')
             {
-                fprintf(file, "Division by zero detected. Exiting.\n");
-                fflush(file);
-                int status_err = -1;
-                fclose(file);
-                exit(EXIT_FAILURE);
+                line[line_pos] = '\0';
+
+                float numbers[100];
+                int count = 0;
+                char *token = strtok(line, " ");
+                while (token && count < 100)
+                {
+                    numbers[count++] = atof(token);
+                    token = strtok(NULL, " ");
+                }
+
+                if (count < 2)
+                {
+                    buffer[1024];
+                    line[1024];
+                    line_pos = 0;
+                    write_string(1, "Error: Need at least 2 numbers\n");
+                    continue;
+                }
+                else
+                {
+                    float dividend = numbers[0];
+                    for (int j = 1; j < count; j++)
+                    {
+                        if (numbers[j] == 0.0f)
+                        {
+                            write_string(file, "Division by zero detected. Exiting.\n");
+                            write_string(1, "Division by zero detected. Exiting.\n");
+                            close(file);
+                            exit(EXIT_FAILURE);
+                        }
+                        char result[64];
+                        float res = dividend / numbers[j];
+                        int len = snprintf(result, sizeof(result), "%.2f / %.2f = %.2f\n", dividend, numbers[j], res);
+                        write(file, result, len);
+                    }
+                }
+                line_pos = 0;
             }
-            float result = dividend / numbers[i];
-            fprintf(file, "%.2f / %.2f = %.2f\n", dividend, numbers[i], result);
+            else
+            {
+                if (line_pos < sizeof(line) - 1)
+                {
+                    line[line_pos++] = buffer[i];
+                }
+            }
         }
-        fflush(file);
     }
 
-    fclose(file);
+    close(file);
     return 0;
 }
--- src/main.c
+++ src/main.c
@@ -1,70 +1,153 @@
-#define _GNU_SOURCE
+// #define _GNU_SOURCE
 #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <sys/wait.h>
+// #include <string.h>
+
+// int main(){
+//     int pipe1[2], pipe2[2];
+//     pid_t pid;
+
+//     char filename[256];
+//     char buffer[1024];
+
+//     if (pipe(pipe1) == -1 || pipe(pipe2) == -1){
+//         perror("pipe error");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     printf("Enter filename: ");
+//     if (fgets(filename, sizeof(filename), stdin) == NULL)
+//     {
+//         perror("fgets");
+//         exit(EXIT_FAILURE);
+//     }
+//     filename[strcspn(filename, "\n")] = 0;
+
+//     pid = fork();
+
+//     if (pid == -1){
+//         perror("fork error");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     if (pid == 0){
+//         close(pipe1[1]);
+//         close(pipe2[0]);
+
+//         if (dup2(pipe1[0], STDIN_FILENO) == -1){
+//             perror("dup2 pipe1[0]");
+//             exit(EXIT_FAILURE);
+//         }
+//         close(pipe1[0]);
+//         execl("./child", "child", filename, NULL);
+//         perror("execl error");
+//         exit(EXIT_FAILURE);
+//     }
+//     else{
+//         close(pipe1[0]);
+//         close(pipe2[1]);
+//         printf("Enter numbers separated by spaces: ");
+//         while (fgets(buffer, sizeof(buffer), stdin) != NULL){
+//             write(pipe1[1], buffer, strlen(buffer));
+
+//             int status;
+//                 pid_t res = waitpid(pid, &status, WNOHANG);
+//                 if (res != 0){
+//                     break;
+//                 }
+
+//                 printf("Enter next numbers: ");
+//             fflush(stdout);
+//         }
+//         close(pipe1[1]);
+//         close(pipe2[0]);
+//         waitpid(pid, NULL, 0);
+//     }
+    
+//     return 0;
+// }
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
 
-int main(){
+void write_string(int fd, const char *str)
+{
+    write(fd, str, strlen(str));
+}
+
+int main()
+{
     int pipe1[2], pipe2[2];
     pid_t pid;
 
     char filename[256];
     char buffer[1024];
+    ssize_t bytes_read;
 
-    if (pipe(pipe1) == -1 || pipe(pipe2) == -1){
-        perror("pipe error");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Enter filename: ");
-    if (fgets(filename, sizeof(filename), stdin) == NULL)
+    if (pipe(pipe1) == -1 || pipe(pipe2) == -1)
     {
-        perror("fgets");
+        write_string(STDERR_FILENO, "pipe error\n");
         exit(EXIT_FAILURE);
     }
-    filename[strcspn(filename, "\n")] = 0;
+
+    write_string(STDOUT_FILENO, "Enter filename: ");
+    bytes_read = read(STDIN_FILENO, filename, sizeof(filename) - 1);
+    if (bytes_read <= 0)
+    {
+        write_string(STDERR_FILENO, "read error\n");
+        exit(EXIT_FAILURE);
+    }
+    filename[bytes_read - 1] = '\0';
 
     pid = fork();
 
-    if (pid == -1){
-        perror("fork error");
+    if (pid == -1)
+    {
+        write_string(STDERR_FILENO, "fork error\n");
         exit(EXIT_FAILURE);
     }
 
-    if (pid == 0){
+    if (pid == 0)
+    {
         close(pipe1[1]);
         close(pipe2[0]);
 
-        if (dup2(pipe1[0], STDIN_FILENO) == -1){
-            perror("dup2 pipe1[0]");
+        if (dup2(pipe1[0], STDIN_FILENO) == -1)
+        {
+            write_string(STDERR_FILENO, "dup2 error\n");
             exit(EXIT_FAILURE);
         }
         close(pipe1[0]);
         execl("./child", "child", filename, NULL);
-        perror("execl error");
+        write_string(STDERR_FILENO, "execl error\n");
         exit(EXIT_FAILURE);
     }
-    else{
+    else
+    {
         close(pipe1[0]);
         close(pipe2[1]);
-        printf("Enter numbers separated by spaces: ");
-        while (fgets(buffer, sizeof(buffer), stdin) != NULL){
-            write(pipe1[1], buffer, strlen(buffer));
+
+        write_string(STDOUT_FILENO, "Enter numbers separated by spaces: ");
+        while ((bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0)
+        {
+            write(pipe1[1], buffer, bytes_read);
 
             int status;
-                pid_t res = waitpid(pid, &status, WNOHANG);
-                if (res != 0){
-                    break;
-                }
+            pid_t res = waitpid(pid, &status, WNOHANG);
+            if (res != 0)
+            {
+                break;
+            }
 
-                printf("Enter next numbers: ");
-            fflush(stdout);
+            write_string(STDOUT_FILENO, "Enter next numbers: ");
         }
         close(pipe1[1]);
         close(pipe2[0]);
         waitpid(pid, NULL, 0);
     }
-    
+
     return 0;
 }
